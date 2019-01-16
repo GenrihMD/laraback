af6fc5cb40c6dc74dd0e347c2b0cab0879d87d9d
Ale Lmn <genrih.md@gmail.com>
2019-01-17 02:29:15 +0300

    create test route and test template

---
+++ resources/views/test.blade.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Test</title>
+
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="/css/app.css">
+</head>
+<body>
+<div class="content">
+    <div class="row">
+        <h1 class="col-6 m-1">
+            Test page
+        </h1>
+    </div>
+</div>
+</body>
+</html>
