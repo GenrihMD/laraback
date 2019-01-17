3acd7c8774f687cc2cd1d47793d985ae25b4ef5f
Ale Lmn <genrih.md@gmail.com>
2019-01-17 18:26:52 +0300

    set up resource files of PIXELS template

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -8,9 +8,16 @@
 
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
     <link rel="stylesheet" href="/css/app.css">
+    <link rel="stylesheet" href="/css/styles.css">
 </head>
 <body>
 <div class="content">
+    <div class="row">
+        <div class="col-2">
+            <img class="logo" src="/svg/logo.svg" alt="logo">
+
+        </div>
+    </div>
     <div class="row">
         <h1 class="col-6 m-1">
             Test page
